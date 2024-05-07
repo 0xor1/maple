491e2dba84f8c747dabad5abecc7b9c5fce39365
Daniel Robinson <dan7c2@gmail.com>
2024-05-07 12:16:10 +0100

    remove strings

--- Maple.I18n/Keys.cs
+++ Maple.I18n/Keys.cs
@@ -1,10 +1,12 @@
 // Generated Code File, Do Not Edit.
-// This file is generated with Common.I18nCodeGen.
+// This file is generated with Common.Cmds.
+// see https://github.com/0xor1/common/blob/main/Common.Cmds/I18n.cs
+// executed with arguments: i18n <abs_file_path_to>/Maple.I18n Maple.I18n false 
 
 namespace Maple.I18n;
 
 public static partial class S
-{
+{ 
     public const string Counter = "counter";
     public const string Decrement = "decrement";
     public const string Decrementing = "decrementing";
--- Maple.I18n/SZLibrary.cs
+++ Maple.I18n/SZLibrary.cs
@@ -1,3 +1,8 @@
+// Generated Code File, Do Not Edit.
+// This file is generated with Common.Cmds.
+// see https://github.com/0xor1/common/blob/main/Common.Cmds/I18n.cs
+// executed with arguments: i18n <abs_file_path_to>/Maple.I18n Maple.I18n false 
+
 using Common.Shared;
 
 namespace Maple.I18n;
@@ -5,12 +10,9 @@ namespace Maple.I18n;
 public static partial class S
 {
     private static readonly Dictionary<string, Dictionary<string, TemplatableString>> Library =
-        new()
+        new ()
         {
+            
             { Common.Shared.I18n.S.EN, EN_Strings },
-            { Common.Shared.I18n.S.ES, ES_Strings },
-            { Common.Shared.I18n.S.FR, FR_Strings },
-            { Common.Shared.I18n.S.DE, DE_Strings },
-            { Common.Shared.I18n.S.IT, IT_Strings }
         };
 }
