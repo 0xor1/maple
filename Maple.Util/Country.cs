c5f5132b73a91a5d254e1674d5c0ec1803cb4ca7
Daniel Robinson <dan7c2@gmail.com>
2024-05-08 17:12:42 +0100

    add countries list

---
+++ Maple.Util/Country.cs
@@ -0,0 +1,401 @@
+﻿using Common.Shared;
+
+namespace Maple.Util;
+
+public static class Country
+{
+	public static readonly Key Afghanistan = new ("afghanistan");
+	public static readonly Key Albania = new ("albania");
+	public static readonly Key Algeria = new ("algeria");
+	public static readonly Key Andorra = new ("andorra");
+	public static readonly Key Angola = new ("angola");
+	public static readonly Key AntiguaAndBarbuda = new ("antigua_and_barbuda");
+	public static readonly Key Argentina = new ("argentina");
+	public static readonly Key Armenia = new ("armenia");
+	public static readonly Key Australia = new ("australia");
+	public static readonly Key Austria = new ("austria");
+	public static readonly Key Azerbaijan = new ("azerbaijan");
+	public static readonly Key Bahamas = new ("bahamas");
+	public static readonly Key Bahrain = new ("bahrain");
+	public static readonly Key Bangladesh = new ("bangladesh");
+	public static readonly Key Barbados = new ("barbados");
+	public static readonly Key Belarus = new ("belarus");
+	public static readonly Key Belgium = new ("belgium");
+	public static readonly Key Belize = new ("belize");
+	public static readonly Key Benin = new ("benin");
+	public static readonly Key Bhutan = new ("bhutan");
+	public static readonly Key Bolivia = new ("bolivia");
+	public static readonly Key BosniaAndHerzegovina = new ("bosnia_and_herzegovina");
+	public static readonly Key Botswana = new ("botswana");
+	public static readonly Key Brazil = new ("brazil");
+	public static readonly Key Brunei = new ("brunei");
+	public static readonly Key Bulgaria = new ("bulgaria");
+	public static readonly Key BurkinaFaso = new ("burkina_faso");
+	public static readonly Key Burundi = new ("burundi");
+	public static readonly Key CaboVerde = new ("cabo_verde");
+	public static readonly Key Cambodia = new ("cambodia");
+	public static readonly Key Cameroon = new ("cameroon");
+	public static readonly Key Canada = new ("canada");
+	public static readonly Key CentralAfricanRepublic = new ("central_african_republic");
+	public static readonly Key Chad = new ("chad");
+	public static readonly Key Chile = new ("chile");
+	public static readonly Key China = new ("china");
+	public static readonly Key Colombia = new ("colombia");
+	public static readonly Key Comoros = new ("comoros");
+	public static readonly Key Congo = new ("congo");
+	public static readonly Key CostaRica = new ("costa_rica");
+	public static readonly Key Croatia = new ("croatia");
+	public static readonly Key Cuba = new ("cuba");
+	public static readonly Key Cyprus = new ("cyprus");
+	public static readonly Key CzechRepublic = new ("czech_republic_czechia");
+	public static readonly Key CoteDIvoire = new ("cote_d_ivoire");
+	public static readonly Key Denmark = new ("denmark");
+	public static readonly Key Djibouti = new ("djibouti");
+	public static readonly Key Dominica = new ("dominica");
+	public static readonly Key DominicanRepublic = new ("dominican_republic");
+	public static readonly Key DrCongo = new ("dr_congo");
+	public static readonly Key Ecuador = new ("ecuador");
+	public static readonly Key Egypt = new ("egypt");
+	public static readonly Key ElSalvador = new ("el_salvador");
+	public static readonly Key EquatorialGuinea = new ("equatorial_guinea");
+	public static readonly Key Eritrea = new ("eritrea");
+	public static readonly Key Estonia = new ("estonia");
+	public static readonly Key Eswatini = new ("eswatini");
+	public static readonly Key Ethiopia = new ("ethiopia");
+	public static readonly Key Fiji = new ("fiji");
+	public static readonly Key Finland = new ("finland");
+	public static readonly Key France = new ("france");
+	public static readonly Key Gabon = new ("gabon");
+	public static readonly Key Gambia = new ("gambia");
+	public static readonly Key Georgia = new ("georgia");
+	public static readonly Key Germany = new ("germany");
+	public static readonly Key Ghana = new ("ghana");
+	public static readonly Key Greece = new ("greece");
+	public static readonly Key Grenada = new ("grenada");
+	public static readonly Key Guatemala = new ("guatemala");
+	public static readonly Key Guinea = new ("guinea");
+	public static readonly Key GuineaBissau = new ("guinea_bissau");
+	public static readonly Key Guyana = new ("guyana");
+	public static readonly Key Haiti = new ("haiti");
+	public static readonly Key HolySee = new ("holy_see");
+	public static readonly Key Honduras = new ("honduras");
+	public static readonly Key Hungary = new ("hungary");
+	public static readonly Key Iceland = new ("iceland");
+	public static readonly Key India = new ("india");
+	public static readonly Key Indonesia = new ("indonesia");
+	public static readonly Key Iran = new ("iran");
+	public static readonly Key Iraq = new ("iraq");
+	public static readonly Key Ireland = new ("ireland");
+	public static readonly Key Israel = new ("israel");
+	public static readonly Key Italy = new ("italy");
+	public static readonly Key Jamaica = new ("jamaica");
+	public static readonly Key Japan = new ("japan");
+	public static readonly Key Jordan = new ("jordan");
+	public static readonly Key Kazakhstan = new ("kazakhstan");
+	public static readonly Key Kenya = new ("kenya");
+	public static readonly Key Kiribati = new ("kiribati");
+	public static readonly Key Kuwait = new ("kuwait");
+	public static readonly Key Kyrgyzstan = new ("kyrgyzstan");
+	public static readonly Key Laos = new ("laos");
+	public static readonly Key Latvia = new ("latvia");
+	public static readonly Key Lebanon = new ("lebanon");
+	public static readonly Key Lesotho = new ("lesotho");
+	public static readonly Key Liberia = new ("liberia");
+	public static readonly Key Libya = new ("libya");
+	public static readonly Key Liechtenstein = new ("liechtenstein");
+	public static readonly Key Lithuania = new ("lithuania");
+	public static readonly Key Luxembourg = new ("luxembourg");
+	public static readonly Key Madagascar = new ("madagascar");
+	public static readonly Key Malawi = new ("malawi");
+	public static readonly Key Malaysia = new ("malaysia");
+	public static readonly Key Maldives = new ("maldives");
+	public static readonly Key Mali = new ("mali");
+	public static readonly Key Malta = new ("malta");
+	public static readonly Key MarshallIslands = new ("marshall_islands");
+	public static readonly Key Mauritania = new ("mauritania");
+	public static readonly Key Mauritius = new ("mauritius");
+	public static readonly Key Mexico = new ("mexico");
+	public static readonly Key Micronesia = new ("micronesia");
+	public static readonly Key Moldova = new ("moldova");
+	public static readonly Key Monaco = new ("monaco");
+	public static readonly Key Mongolia = new ("mongolia");
+	public static readonly Key Montenegro = new ("montenegro");
+	public static readonly Key Morocco = new ("morocco");
+	public static readonly Key Mozambique = new ("mozambique");
+	public static readonly Key Myanmar = new ("myanmar");
+	public static readonly Key Namibia = new ("namibia");
+	public static readonly Key Nauru = new ("nauru");
+	public static readonly Key Nepal = new ("nepal");
+	public static readonly Key Netherlands = new ("netherlands");
+	public static readonly Key NewZealand = new ("new_zealand");
+	public static readonly Key Nicaragua = new ("nicaragua");
+	public static readonly Key Niger = new ("niger");
+	public static readonly Key Nigeria = new ("nigeria");
+	public static readonly Key NorthKorea = new ("north_korea");
+	public static readonly Key NorthMacedonia = new ("north_macedonia");
+	public static readonly Key Norway = new ("norway");
+	public static readonly Key Oman = new ("oman");
+	public static readonly Key Pakistan = new ("pakistan");
+	public static readonly Key Palau = new ("palau");
+	public static readonly Key Panama = new ("panama");
+	public static readonly Key PapuaNewGuinea = new ("papua_new_guinea");
+	public static readonly Key Paraguay = new ("paraguay");
+	public static readonly Key Peru = new ("peru");
+	public static readonly Key Philippines = new ("philippines");
+	public static readonly Key Poland = new ("poland");
+	public static readonly Key Portugal = new ("portugal");
+	public static readonly Key Qatar = new ("qatar");
+	public static readonly Key Romania = new ("romania");
+	public static readonly Key Russia = new ("russia");
+	public static readonly Key Rwanda = new ("rwanda");
+	public static readonly Key SaintKittsAndNevis = new ("saint_kitts_and_nevis");
+	public static readonly Key SaintLucia = new ("saint_lucia");
+	public static readonly Key Samoa = new ("samoa");
+	public static readonly Key SanMarino = new ("san_marino");
+	public static readonly Key SaoTomeAndPrincipe = new ("sao_tome_and_principe");
+	public static readonly Key SaudiArabia = new ("saudi_arabia");
+	public static readonly Key Senegal = new ("senegal");
+	public static readonly Key Serbia = new ("serbia");
+	public static readonly Key Seychelles = new ("seychelles");
+	public static readonly Key SierraLeone = new ("sierra_leone");
+	public static readonly Key Singapore = new ("singapore");
+	public static readonly Key Slovakia = new ("slovakia");
+	public static readonly Key Slovenia = new ("slovenia");
+	public static readonly Key SolomonIslands = new ("solomon_islands");
+	public static readonly Key Somalia = new ("somalia");
+	public static readonly Key SouthAfrica = new ("south_africa");
+	public static readonly Key SouthKorea = new ("south_korea");
+	public static readonly Key SouthSudan = new ("south_sudan");
+	public static readonly Key Spain = new ("spain");
+	public static readonly Key SriLanka = new ("sri_lanka");
+	public static readonly Key StVincentAndGrenadines = new ("st_vincent_and_grenadines");
+	public static readonly Key StateOfPalestine = new ("state_of_palestine");
+	public static readonly Key Sudan = new ("sudan");
+	public static readonly Key Suriname = new ("suriname");
+	public static readonly Key Sweden = new ("sweden");
+	public static readonly Key Switzerland = new ("switzerland");
+	public static readonly Key Syria = new ("syria");
+	public static readonly Key Tajikistan = new ("tajikistan");
+	public static readonly Key Tanzania = new ("tanzania");
+	public static readonly Key Thailand = new ("thailand");
+	public static readonly Key TimorLeste = new ("timor_leste");
+	public static readonly Key Togo = new ("togo");
+	public static readonly Key Tonga = new ("tonga");
+	public static readonly Key TrinidadAndTobago = new ("trinidad_and_tobago");
+	public static readonly Key Tunisia = new ("tunisia");
+	public static readonly Key Turkey = new ("turkey");
+	public static readonly Key Turkmenistan = new ("turkmenistan");
+	public static readonly Key Tuvalu = new ("tuvalu");
+	public static readonly Key Uganda = new ("uganda");
+	public static readonly Key Ukraine = new ("ukraine");
+	public static readonly Key UnitedArabEmirates = new ("united_arab_emirates");
+	public static readonly Key UnitedKingdom = new ("united_kingdom");
+	public static readonly Key UnitedStates = new ("united_states");
+	public static readonly Key Uruguay = new ("uruguay");
+	public static readonly Key Uzbekistan = new ("uzbekistan");
+	public static readonly Key Vanuatu = new ("vanuatu");
+	public static readonly Key Venezuela = new ("venezuela");
+	public static readonly Key Vietnam = new ("vietnam");
+	public static readonly Key Yemen = new ("yemen");
+	public static readonly Key Zambia = new ("zambia");
+	public static readonly Key Zimbabwe = new ("zimbabwe");
+
+	public static readonly IEnumerable<Key> Enum = new List<Key>()
+	{ 
+		Afghanistan,
+		Albania,
+		Algeria,
+		Andorra,
+		Angola,
+		AntiguaAndBarbuda,
+		Argentina,
+		Armenia,
+		Australia,
+		Austria,
+		Azerbaijan,
+		Bahamas,
+		Bahrain,
+		Bangladesh,
+		Barbados,
+		Belarus,
+		Belgium,
+		Belize,
+		Benin,
+		Bhutan,
+		Bolivia,
+		BosniaAndHerzegovina,
+		Botswana,
+		Brazil,
+		Brunei,
+		Bulgaria,
+		BurkinaFaso,
+		Burundi,
+		CaboVerde,
+		Cambodia,
+		Cameroon,
+		Canada,
+		CentralAfricanRepublic,
+		Chad,
+		Chile,
+		China,
+		Colombia,
+		Comoros,
+		Congo,
+		CostaRica,
+		Croatia,
+		Cuba,
+		Cyprus,
+		CzechRepublic,
+		CoteDIvoire,
+		Denmark,
+		Djibouti,
+		Dominica,
+		DominicanRepublic,
+		DrCongo,
+		Ecuador,
+		Egypt,
+		ElSalvador,
+		EquatorialGuinea,
+		Eritrea,
+		Estonia,
+		Eswatini,
+		Ethiopia,
+		Fiji,
+		Finland,
+		France,
+		Gabon,
+		Gambia,
+		Georgia,
+		Germany,
+		Ghana,
+		Greece,
+		Grenada,
+		Guatemala,
+		Guinea,
+		GuineaBissau,
+		Guyana,
+		Haiti,
+		HolySee,
+		Honduras,
+		Hungary,
+		Iceland,
+		India,
+		Indonesia,
+		Iran,
+		Iraq,
+		Ireland,
+		Israel,
+		Italy,
+		Jamaica,
+		Japan,
+		Jordan,
+		Kazakhstan,
+		Kenya,
+		Kiribati,
+		Kuwait,
+		Kyrgyzstan,
+		Laos,
+		Latvia,
+		Lebanon,
+		Lesotho,
+		Liberia,
+		Libya,
+		Liechtenstein,
+		Lithuania,
+		Luxembourg,
+		Madagascar,
+		Malawi,
+		Malaysia,
+		Maldives,
+		Mali,
+		Malta,
+		MarshallIslands,
+		Mauritania,
+		Mauritius,
+		Mexico,
+		Micronesia,
+		Moldova,
+		Monaco,
+		Mongolia,
+		Montenegro,
+		Morocco,
+		Mozambique,
+		Myanmar,
+		Namibia,
+		Nauru,
+		Nepal,
+		Netherlands,
+		NewZealand,
+		Nicaragua,
+		Niger,
+		Nigeria,
+		NorthKorea,
+		NorthMacedonia,
+		Norway,
+		Oman,
+		Pakistan,
+		Palau,
+		Panama,
+		PapuaNewGuinea,
+		Paraguay,
+		Peru,
+		Philippines,
+		Poland,
+		Portugal,
+		Qatar,
+		Romania,
+		Russia,
+		Rwanda,
+		SaintKittsAndNevis,
+		SaintLucia,
+		Samoa,
+		SanMarino,
+		SaoTomeAndPrincipe,
+		SaudiArabia,
+		Senegal,
+		Serbia,
+		Seychelles,
+		SierraLeone,
+		Singapore,
+		Slovakia,
+		Slovenia,
+		SolomonIslands,
+		Somalia,
+		SouthAfrica,
+		SouthKorea,
+		SouthSudan,
+		Spain,
+		SriLanka,
+		StVincentAndGrenadines,
+		StateOfPalestine,
+		Sudan,
+		Suriname,
+		Sweden,
+		Switzerland,
+		Syria,
+		Tajikistan,
+		Tanzania,
+		Thailand,
+		TimorLeste,
+		Togo,
+		Tonga,
+		TrinidadAndTobago,
+		Tunisia,
+		Turkey,
+		Turkmenistan,
+		Tuvalu,
+		Uganda,
+		Ukraine,
+		UnitedArabEmirates,
+		UnitedKingdom,
+		UnitedStates,
+		Uruguay,
+		Uzbekistan,
+		Vanuatu,
+		Venezuela,
+		Vietnam,
+		Yemen,
+		Zambia,
+		Zimbabwe,
+	};
+}
